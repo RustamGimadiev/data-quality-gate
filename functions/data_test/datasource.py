42ef98b5ddeed9c8d00e4bd0afb45bec67190412
bvolodarskiy <bvolodarskiy@provectus.com>
2022-09-07 12:40:48 +0300

    added csv reader

--- functions/data_test/datasource.py
+++ functions/data_test/datasource.py
@@ -5,7 +5,8 @@ import boto3
 import awswrangler as wr
 import re
 
-def concat_source_list(engine,source,source_engine):
+
+def concat_source_list(engine, source, source_engine):
     final_source_files = []
     if engine == 's3':
         for sc in source:
@@ -23,28 +24,20 @@ def concat_source_list(engine,source,source_engine):
         return 6
     else:
         for sc in source:
-            final_source_files.append('s3://'+source_engine + '/' + sc)
+            final_source_files.append('s3://' + source_engine + '/' + sc)
         return final_source_files
 
-def prepare_source(engine,source,source_engine):
-    if engine == 's3':
-        return 's3://'+source_engine + '/' + source
-    elif engine == 'athena':
-        return 2
-    elif engine == 'redshift':
-        return 3
-    elif engine == 'hudi':
-        return 4
-    elif engine == 'postgresql':
-        return 5
-    elif engine == 'snowflake':
-        return 6
-    else:
-        return 's3://'+source_engine + '/' + source
 
-def read_source(source,engine):
+def get_file_extension(source):
+    return source.split(".")[-1]
+
+
+def read_source(source, engine, extension):
     if engine == 's3':
-        return wr.s3.read_parquet(path=source)
+        if extension == 'csv':
+            return wr.s3.read_csv(path=source)
+        elif extension == 'parquet':
+            return wr.s3.read_parquet(path=source)
     elif engine == 'athena':
         return 2
     elif engine == 'redshift':
@@ -58,13 +51,13 @@ def read_source(source,engine):
     else:
         return wr.s3.read_parquet(path=source)
 
-def get_source_name(source,engine):
+
+def get_source_name(source, engine,extension):
     if engine == 's3':
-        if type(source) == list:
-            source_name = re.search('.*/(.+?)(\_(\d.*)|).parquet', source[0]).group(1)
-        else:
-            source_name = re.search('.*/(.+?)(\_(\d.*)|).parquet', source).group(1)
-        return source_name
+        if extension == 'csv':
+            return re.search('.*/(.+?)(\_(\d.*)|).csv', source).group(1)
+        elif extension == 'parquet':
+            return re.search('.*/(.+?)(\_(\d.*)|).parquet', source).group(1)
     elif engine == 'athena':
         return 2
     elif engine == 'redshift':
@@ -76,19 +69,14 @@ def get_source_name(source,engine):
     elif engine == 'snowflake':
         return 6
     else:
-        if type(source) == list:
-            source_name = re.search('.*/(.+?)(\_(\d.*)|).parquet', source[0]).group(1)
-        else:
-            source_name = re.search('.*/(.+?)(\_(\d.*)|).parquet', source).group(1)
-        return source_name
+        return re.search('.*/(.+?)(\_(\d.*)|).parquet', source).group(1)
 
-def prepare_final_ds(source,engine,source_engine,source_name):
-    if (source_name == ''):
-        source_name = get_source_name(source, engine)
-    if type(source) == list:
-        source = concat_source_list(engine,source,source_engine)
-    else:
-        source = prepare_source(engine,source,source_engine)
-    df = read_source(source,engine)
 
-    return df,source_name
+def prepare_final_ds(source, engine, source_engine, source_name):
+    source = concat_source_list(engine, source, source_engine)
+    source_extension = get_file_extension(source[0])
+    if source_name == '':
+        source_name = get_source_name(source[0], engine, source_extension)
+    df = read_source(source, engine, source_extension)
+
+    return df, source_name
